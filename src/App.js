d2bb1bd0b9beb32550b749fe39d6945b719bff78
itsprakashm <51751954+itsprakashm@users.noreply.github.com>
2020-09-26 18:16:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 function App() {
+  function responseGoogle(response){
+    console.log(response);
+  }
+  function responseFacebook(response){
+    console.log(response);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="auth-wrapper">
+      <div className="auth-inner">
+            <form>
+                <h3 className="centertext">Sign Up</h3>
+
+                <div className="">
+                  <h2 className="centertext">Create your account</h2>
+                  <p className="centertext">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+                <div className="row">
+                <div className="col-lg-6">
+                <div className="pull-left">
+                <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Sign up google"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+  </div>
+  </div>
+  <div className="col-lg-6">
+  <div className="pull-right">
+  <FacebookLogin
+    appId="1088597931155576"
+    autoLoad={true}
+    fields="name,email,picture"
+    callback={responseFacebook}
+    cssClass="my-facebook-button-class"
+    icon="fa-facebook"
+    textButton="Sign up with facebook"
+  />
+  </div>
+  </div>
+  </div>
+  <hr/>
+
+                <div className="form-group">
+                    
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                <p className="terms">By clicking sign up you agree to our term and condition</p>
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+            </form>
+          </div>
+        </div>
     </div>
   );
 }
